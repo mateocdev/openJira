c9faf7b363c3a18086193a7f4ebdb97d5729a841
mateo <m.castano1@uniandes.edu.co>
2023-01-08 18:52:26 -0500

    💅[ADD] Style for index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
-import { Typography } from "@mui/material";
 import type { NextPage } from "next";
+import { Card, CardContent, CardHeader, Grid } from "@mui/material";
 import { Layout } from "../components/layouts";
 
 const HomePage: NextPage = () => {
   return (
-    <Layout title="Home">
-      <Typography variant="h1" color="primary">
-        Hello World🌎
-      </Typography>
+    <Layout title="Home - OpenJira">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px);" }}>
+            <CardHeader title="Pendientes" />
+
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px);" }}>
+            <CardHeader title="En progreso" />
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px);" }}>
+            <CardHeader title="Completadas" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
