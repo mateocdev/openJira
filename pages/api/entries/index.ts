8fd4c55ee2b23229b21b690accc851335bda18f0
MateoCastanoG <m.castano1@uniandes.edu.co>
2023-02-20 17:12:14 -0500

    🚧[ADD]EndPoint on next.js to consume on the client with the entries

---
+++ pages/api/entries/index.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database';
+import { Entry, IEntry } from '../../../models';
+
+type Data = {
+    message: string
+} | IEntry[]
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+    switch (req.method) {
+        case 'GET':
+            return getEntries(res);
+            break;
+    
+        default:
+            return res.status(400).json({ message: 'Bad Request' })
+    }
+
+}
+
+const getEntries = async(res: NextApiResponse<Data>) => {
+    await db.connectToDatabase();
+    const entries = await Entry.find().sort({ createdAt: 'ascending' });
+    await db.disconnectDatabase();
+
+    return res.status(200).json(entries);
+}
