6243bd1e3c436080b41148afc79269360fcd693f
MateoCastanoG <m.castano1@uniandes.edu.co>
2023-03-15 23:02:08 -0500

    🚧[ADD]Update entry backend

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../../database";
+import { Entry, IEntry } from "../../../models";
+
+type Data =
+  | IEntry
+  | {
+      message: string;
+    };
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // Always is a string
+  const { id } = req.query;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({ message: "Invalid ID:" + id });
+  }
+
+  switch (req.method) {
+    case "PUT":
+      return updateEntry(req, res);
+    default:
+      return res.status(405).json({ message: "Method not allowed" });
+  }
+}
+
+const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { id } = req.query;
+  await db.connectToDatabase();
+  const entryToUpdate = await Entry.findById(id);
+
+  if (!entryToUpdate) {
+    await db.disconnectDatabase();
+    return res.status(404).json({ message: "Entry not found" });
+  }
+
+  const {
+    description = entryToUpdate.description,
+    status = entryToUpdate.status,
+  } = req.body;
+
+  try {
+    const updatedEntry = await Entry.findByIdAndUpdate(
+      id,
+      {
+        description,
+        status,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    await db.disconnectDatabase();
+    res.status(200).json(updatedEntry!);
+  } catch ({ errors }: any) {
+    await db.disconnectDatabase();
+    return res.status(400).json({ message: errors.status.message.toString() });
+  }
+};
