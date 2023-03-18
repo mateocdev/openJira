fcd0a875564a212fff6f07848658601113a7a68a
MateoCastanoG <m.castano1@uniandes.edu.co>
2023-03-17 19:29:56 -0500

    [WIP]Adding middleware

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+// middleware.ts
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.startsWith("/api/entries/")) {
+    const id = req.nextUrl.pathname.split("/").pop();
+    console.log({ id });
+    const checkMongoIDRegex = new RegExp("^[0-9a-fA-F]{24}$");
+    if (!checkMongoIDRegex.test(id)) {
+      const url = req.nextUrl.clone();
+      url.pathname = "/api/bad-request";
+      url.search = `?message=${id} is not a valid ID`;
+      return NextResponse.rewrite(url);
+    }
+  }
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/api/entries/:path*"],
+};
---
+++ pages/api/bad-request.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  ok: boolean;
+  message: string | string[];
+};
+
+export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { message = "Bad Request" } = req.query;
+  res.status(400).json({ ok: false, message });
+}
